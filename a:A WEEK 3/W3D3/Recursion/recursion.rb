cbdcbb7eeeca982147d55c9c487dfae563a143ea
Tony Zhang <tozhang665@gmail.com>
2021-07-21 11:50:46 -0700

    added binary search

--- a:A WEEK 3/W3D3/Recursion/recursion.rb
+++ a:A WEEK 3/W3D3/Recursion/recursion.rb
@@ -51,8 +51,52 @@ class Array
 
 end
 
-arr = [[1,2,3],[4,5,6]]
+# arr = [[1,2,3],[4,5,6]]
 
-d = arr.deep_dup
+# d = arr.deep_dup
 
-p d
+# p d
+
+
+def rec_fibonacci(n)
+    return nil if n == 0
+    return 1 if n == 1
+    return 1 if n == 2
+    arr = [1,1]
+    rec_fibonacci(n-1) + rec_fibonacci(n-2)
+end
+
+
+def iter_fibonacci(n)
+    return nil if n < 1
+    return [1] if n == 1
+    return [1,1] if n == 2
+    arr = [1,1]
+    while arr.length < n
+        arr << arr[-1] + arr[-2]
+    end
+    arr
+end
+
+# p rec_fibonacci(10)
+# p iter_fibonacci(10)
+
+def bsearch(arr,target)
+    return nil if arr.length == 0
+    if arr[-1] == target
+        return arr.length-1
+    else
+        bsearch(arr[0..-2],target)
+    end
+end
+
+
+# p bsearch([1, 2, 3], 1) # => 0
+# p bsearch([2, 3, 4, 5], 3) # => 1
+# p bsearch([2, 4, 6, 8, 10], 6) # => 2
+# p bsearch([1, 3, 4, 5, 9], 5) # => 3
+# p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
+# p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
+# p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
+
+
