f3f75b0bc6e590dabdf29e98db767527f4ac4d2c
Tony Zhang <tozhang665@gmail.com>
2021-07-21 16:50:31 -0700

    added word Chain Phase 1

---
+++ a:A WEEK 3/W3D3/WordChains/wordChain.rb
@@ -0,0 +1,39 @@
+require 'set'
+
+
+class WordChainer
+
+    attr_reader :dictionary
+
+    def initialize
+        @dictionary = Set.new([])
+        File.open('dictionary.txt').each do |line|
+            @dictionary.add(line.chomp())
+        end
+    end
+
+    def adjacent_words(word)
+        adjacent_set = Set.new([])
+        alpha = ('a'..'z').to_a
+        word.each_char.with_index do |char,idx|
+            alpha.each do |alphabet|
+                portion = word+""
+                portion[idx] = alphabet
+                if @dictionary.include?(portion)
+                    adjacent_set.add(portion)
+                end
+            end
+            word
+        end
+        adjacent_set.delete(word)
+        adjacent_set
+    end
+end
+
+
+# d = WordChainer.new
+
+# # p d.dictionary
+# #axis
+# p d.adjacent_words("cat")
+
