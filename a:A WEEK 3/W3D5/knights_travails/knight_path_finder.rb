eef024575c3fc7f83430fb033d1ca37f3f40c4d4
Tony Zhang <tozhang665@gmail.com>
2021-07-26 11:00:45 -0700

    finished Knights Travail pt 2

--- a:A WEEK 3/W3D5/knights_travails/knight_path_finder.rb
+++ a:A WEEK 3/W3D5/knights_travails/knight_path_finder.rb
@@ -33,16 +33,70 @@ class Knight
     def initialize(pos)
       @base_pos = pos
       @considered_positions = []
+      @root_node = PolyTreeNode.new(pos)
+      build_move_tree
     end
 
 
     def new_move_positions(pos)
       new_positions = Knight.valid_moves(pos).select {|val| !@considered_positions.include?(val)}
-      @considered_positions += new_positions
+      @considered_positions = @considered_positions + new_positions
       return new_positions
     end
     
+    def build_move_tree
+      nodes = [self.root_node]
+      until nodes.empty?
+        currentNode = nodes.shift
+        positions = new_move_positions(currentNode.value)
+        positions.each do |currentPOS|
+          newNode = PolyTreeNode.new(currentPOS)
+          currentNode.add_child(newNode)
+          nodes << newNode
+        end
+      end
+    end
+
+    # def find_path(end_node)
+    #   nodes = []
+    #   current_node = end_node
+    #   until current_node.nil?
+    #     nodes << current_node
+    #     current_node = current_node.parent
+    #   end
+    #   nodes.map{|node| node.value}
+    # end
 
 
+    def find_path(target)
+      end_node = self.root_node.bfs(target)
+      self.trace_path_back(end_node)
+    end
+
+    def trace_path_back(node)
+      # end_node = self.root_node.dfs(node)
+      # self.find_path(end_node)
+
+      nodes = []
+      current_node = node
+      until current_node.nil?
+        nodes << current_node
+        current_node = current_node.parent
+      end
+      nodes.map{|node| node.value}.reverse
+    end
 
 end
+
+
+
+# def bfs(target_value) #Queue
+#     stack = [self]
+#     until stack.empty?
+#       ele = stack.shift
+#       # return ele.pastPos if ele.value == target
+#       return ele if ele.value == target_value
+#       stack += ele.children
+#     end
+#     nil
+#   end 
