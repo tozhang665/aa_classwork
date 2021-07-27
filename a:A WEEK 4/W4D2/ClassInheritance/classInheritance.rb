8bff36e7177467f5410caba561711f50c250c022
Tony Zhang <tozhang665@gmail.com>
2021-07-27 13:46:13 -0700

    commit current

--- a:A WEEK 4/W4D2/ClassInheritance/classInheritance.rb
+++ a:A WEEK 4/W4D2/ClassInheritance/classInheritance.rb
@@ -1,7 +1,24 @@
 class Employee
+    def initialize(name)
+        @name = name
+    end
 
+    def scream
+        p "AAAAAARRRGGGHHHHH GET ME OUT OF HEERREEEE"
+    end
 end
 
 class Manager < Employee
+    def initialize(name)
+        super
+    end
+    def scream
+        p "AAHHHHHHH"
+    end
+end
 
+class Ceo < Employee
+    def relax
+        p "i just get paid"
+    end
 end
