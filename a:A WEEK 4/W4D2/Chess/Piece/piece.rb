8efdf4d5dedac505e4c89aab3cd2571b6e46a815
Anusha Tuladhar <atuladhar@ucdavis.edu>
2021-07-27 17:30:28 -0700

    part 2 chess

--- a:A WEEK 4/W4D2/Chess/Piece/piece.rb
+++ a:A WEEK 4/W4D2/Chess/Piece/piece.rb
@@ -4,12 +4,30 @@ class Piece
         @board = board
         @pos = pos
     end
+
+    def moves 
+        possible_pos = []
+        length1 = @board.grid[0].length
+        length2 = @board.grid.length
+       (0...length1) do |i|
+         (0...length2) do |j|
+            if @board.grid[i][j].color != @color
+                possible_pos << [i, j]
+            end 
+         end 
+          
+       end
+           return possible_pos
+    end
+
     def to_s
 
     end
+
     def empty?
 
     end
+
     def valid_moves
 
     end
@@ -25,6 +43,7 @@ class Piece
     def inspect
         return self.class
     end
+    
 
     private
     def move_into_check?(end_pos)
