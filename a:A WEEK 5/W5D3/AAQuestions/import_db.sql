64ee259c61a07e9a8ee15fbc19814c46520708a7
Tony Zhang <tozhang665@gmail.com>
2021-08-04 15:14:00 -0700

    finished Easy SQL Probs

--- a:A WEEK 5/W5D3/AAQuestions/import_db.sql
+++ a:A WEEK 5/W5D3/AAQuestions/import_db.sql
@@ -1,11 +1,12 @@
 PRAGMA foreign_keys = ON;
-
-DROP TABLE IF EXISTS replies;
-DROP TABLE IF EXISTS questions_follows;
+DROP TABLE IF EXISTS question_follows;
 DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS replies;
 
 DROP TABLE IF EXISTS questions;
+
 DROP TABLE IF EXISTS users;
+
 
 
 CREATE TABLE users( 
@@ -14,7 +15,7 @@ CREATE TABLE users(
   lname TEXT NOT NULL
 );
 
-DROP TABLE IF EXISTS questions;
+-- DROP TABLE IF EXISTS questions;
 CREATE TABLE questions(
   id INTEGER PRIMARY KEY, -- 1 2 3 4 5 6 7 8 9
   title TEXT NOT NULL,
@@ -39,8 +40,9 @@ CREATE TABLE replies(
   body TEXT NOT NULL,
 
   FOREIGN KEY(question_id) REFERENCES questions(id),
-  FOREIGN KEY(parent_id) REFERENCES replies(id),
-  FOREIGN KEY(user_id) REFERENCES users(id)
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  FOREIGN KEY(parent_id) REFERENCES replies(id)
+  
 );
 
 
@@ -65,8 +67,13 @@ INSERT INTO
   questions(title, body, author_id)
 VALUES
   ('Help', 'help please', (SELECT id FROM users WHERE fname = 'Joe' AND lname = 'Mama')),
+  ('Help me too', 'hurry up', (SELECT id FROM users WHERE fname = 'Joe' AND lname = 'Mama')),
+  ('Help me three', 'whats taking so long', (SELECT id FROM users WHERE fname = 'Joe' AND lname = 'Mama')),
   ('How to cook', 'cook ramen', (SELECT id FROM users WHERE fname = 'Kent' AND lname = 'Zhang')),
   ('How to solve', 'cant figure it out', (SELECT id FROM users WHERE fname = 'Tony' AND lname = 'Zhang'));
+
+
+
 
 INSERT INTO
   question_follows(user_id, question_id)
@@ -80,12 +87,30 @@ VALUES
   ((SELECT id FROM users WHERE fname = 'Tony' AND lname = 'Zhang'),
   (SELECT id FROM questions WHERE title = 'How to cook'));
 
+
+
+
+
 INSERT INTO
   replies(question_id, parent_id, user_id, body)
 VALUES
   ((SELECT id FROM questions WHERE title = 'Help'), NULL,
   (SELECT id FROM users WHERE fname = 'Kent' AND lname = 'Zhang'),
   'How can you not know foo'),
+
+  ((SELECT id FROM questions WHERE title = 'Help me too'), NULL,
+  (SELECT id FROM users WHERE fname = 'Kent' AND lname = 'Zhang'),
+  'please'),
+
+  ((SELECT id FROM questions WHERE title = 'Help me too'),
+  1,
+  (SELECT id FROM users WHERE fname = 'Kent' AND lname = 'Zhang'),
+  'woowowwowow'),
+
+
+  ((SELECT id FROM questions WHERE title = 'Help me three'), NULL,
+  (SELECT id FROM users WHERE fname = 'Kent' AND lname = 'Zhang'),
+  'HOW CAN YOU DO THIS TO ME'),
 
   ((SELECT id FROM questions WHERE title = 'How to cook'), NULL,
   (SELECT id FROM users WHERE fname = 'Tony' AND lname = 'Zhang'),
@@ -94,6 +119,10 @@ VALUES
   ((SELECT id FROM questions WHERE title = 'How to solve'), NULL,
   (SELECT id FROM users WHERE fname = 'Tony' AND lname = 'Stark'),
   'please help');
+
+
+
+
 
   INSERT INTO
     question_likes(user_id, question_id)
--- a:A WEEK 5/W5D3/AAQuestions/questionsdatabase.rb
+++ a:A WEEK 5/W5D3/AAQuestions/questionsdatabase.rb
@@ -13,13 +13,6 @@ end
 
 class Users
   attr_accessor :id, :fname, :lname
-
-  def initialize(options)
-    @id = options['id']
-    @fname = options['fname']
-    @lname = options['lname']
-  end
-
   #returns a user variable when searching ID. RETURNS NIL IF ID DOESNT EXIST
   def self.find_by_id(id)
     user = QuestionsDatabase.instance.execute(<<-SQL, id)
@@ -31,8 +24,35 @@ class Users
         id = ?
     SQL
     return nil if user.length < 1
-    user
     Users.new(user[0])
+  end
+
+  def self.find_by_name(fname,lname)
+    user = QuestionsDatabase.instance.execute(<<-SQL, fname,lname)
+    SELECT
+      *
+    FROM
+      users
+    WHERE
+      fname = ? AND lname = ?
+    SQL
+    return nil if user.length < 1
+    Users.new(user[0])
+  end
+  
+  def initialize(options)
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end
+
+
+  def authored_questions
+    Questions.find_by_author_id(self.id)
+  end
+
+  def authored_replies
+    Replies.find_by_user_id(self.id)
   end
 end
 
@@ -40,13 +60,6 @@ end
 
 class Questions
   attr_accessor :id, :title, :body, :author_id
-
-  def initialize(options)
-    @id = options['id']
-    @title = options['title']
-    @body = options['body']
-    @author_id = options['author_id']
-  end
 
   def self.find_by_id(id)
     question = QuestionsDatabase.instance.execute(<<-SQL, id)
@@ -60,6 +73,36 @@ class Questions
     return nil if question.length < 1
     Questions.new(question[0])
   end
+
+  def self.find_by_author_id(author_id)
+    question = QuestionsDatabase.instance.execute(<<-SQL,author_id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        author_id = ?
+    SQL
+    return nil if question.length < 1
+    question.map{|ele| Questions.new(ele)}
+    question
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @author_id = options['author_id']
+  end
+
+  def author
+    Users.find_by_id(self.author_id)
+  end
+
+  def replies
+    Replies.find_by_question_id(self.id)
+  end
+  
 end
 
 class QuestionFollows
@@ -86,14 +129,6 @@ end
 
 class Replies
   attr_accessor :id, :question_id, :parent_id, :user_id, :body
-  def initialize(options)
-    @id = options['id']
-    @question_id = options['question_id']
-    @parent_id = options['parent_id']
-    @user_id = options['user_id']
-    @body = options['body']
-  end
-
   def self.find_by_id(id)
     reply = QuestionsDatabase.instance.execute(<<-SQL, id)
       SELECT
@@ -105,6 +140,67 @@ class Replies
     SQL
     return nil if reply.length < 1
     Replies.new(reply[0])
+  end
+
+  def self.find_by_user_id(user_id)
+    reply = QuestionsDatabase.instance.execute(<<-SQL,user_id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        user_id = ?
+    SQL
+    return nil if reply.length < 1
+    reply.map{|ele| Replies.new(ele)}
+  end
+
+  def self.find_by_question_id(question_id)
+    reply = QuestionsDatabase.instance.execute(<<-SQL,question_id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        question_id = ?
+    SQL
+    return nil if reply.length < 1
+    reply.map{|ele| Replies.new(ele)}
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @question_id = options['question_id']
+    @parent_id = options['parent_id']
+    @user_id = options['user_id']
+    @body = options['body']
+  end
+
+  def author
+    Users.find_by_id(self.user_id)
+  end
+
+  def question
+    Questions.find_by_id(self.question_id)
+  end
+
+  def parent_reply
+    return nil if self.parent_id == nil
+    Replies.find_by_id(self.parent_id)
+  end
+
+  def child_replies
+    reply = QuestionsDatabase.instance.execute(<<-SQL, self.id)
+    SELECT
+    *
+    FROM
+    replies
+    WHERE
+      parent_id = ?
+    SQL
+
+    return nil if reply.length < 1
+    reply.map{|ele| Replies.new(ele)}
   end
 end
 
