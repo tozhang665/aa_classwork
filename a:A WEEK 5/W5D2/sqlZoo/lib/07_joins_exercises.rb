7695906d45069c29f5a1e7be4d66d861b2788905
Tony Zhang <tozhang665@gmail.com>
2021-08-03 17:49:38 -0700

    Update 07_joins_exercises.rb

--- a:A WEEK 5/W5D2/sqlZoo/lib/07_joins_exercises.rb
+++ a:A WEEK 5/W5D2/sqlZoo/lib/07_joins_exercises.rb
@@ -40,6 +40,11 @@ end
 def ford_films
   # List the films in which 'Harrison Ford' has appeared.
   execute(<<-SQL)
+  SELECT title
+  FROM movies
+  JOIN castings ON movies.id = castings.movie_id
+  JOIN actors ON castings.actor_id = actors.id
+  WHERE actors.name = 'Harrison Ford'
   SQL
 end
 
@@ -48,12 +53,22 @@ def ford_supporting_films
   # role. [Note: the ord field of casting gives the position of the actor. If
   # ord=1 then this actor is in the starring role]
   execute(<<-SQL)
+  SELECT title
+  FROM movies
+  JOIN castings ON movies.id = castings.movie_id
+  JOIN actors ON castings.actor_id = actors.id
+  WHERE actors.name = 'Harrison Ford' AND ord != 1
   SQL
 end
 
 def films_and_stars_from_sixty_two
   # List the title and leading star of every 1962 film.
   execute(<<-SQL)
+  SELECT title,name
+  FROM movies
+  JOIN castings ON movies.id = castings.movie_id
+  JOIN actors ON castings.actor_id = actors.id
+  WHERE yr = 1962 AND ord = 1
   SQL
 end
 
@@ -61,6 +76,13 @@ def travoltas_busiest_years
   # Which were the busiest years for 'John Travolta'? Show the year and the
   # number of movies he made for any year in which he made at least 2 movies.
   execute(<<-SQL)
+  SELECT yr, COUNT(*)
+  FROM movies
+  JOIN castings ON movies.id = castings.movie_id
+  JOIN actors ON castings.actor_id = actors.id
+  WHERE name = 'John Travolta'
+  GROUP BY yr
+  HAVING COUNT(*) >=2 
   SQL
 end
 
@@ -68,6 +90,19 @@ def andrews_films_and_leads
   # List the film title and the leading actor for all of the films 'Julie
   # Andrews' played in.
   execute(<<-SQL)
+  SELECT title,name
+  FROM
+    (
+    SELECT title AS base_title
+    FROM movies
+    JOIN castings ON movies.id = castings.movie_id
+    JOIN actors ON castings.actor_id = actors.id
+    WHERE name = 'Julie Andrews'
+    ) AS selected_movies
+  JOIN movies ON selected_movies.base_title = movies.title
+  JOIN castings ON movies.id = castings.movie_id
+  JOIN actors ON castings.actor_id = actors.id
+  WHERE ord = 1
   SQL
 end
 
