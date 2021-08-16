438244a039ae8d2cb42043226fda28ecdeb5acf6
Danny Park <danincali@gmail.com>
2021-08-16 15:45:05 -0700

    session_controller

--- a:A Week 7/W7D1/99Cats-auth/app/controllers/application_controller.rb
+++ a:A Week 7/W7D1/99Cats-auth/app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::Base
+    # crrlll
+
+    helper_method :current_user, :logged_in?
+
+    def current_user
+        # return current user
+            # finds session token and check if it is equal to cookie's session token
+        @current_user ||= User.find_by(session_token:session[:session_token])
+    end
+
+    def require_logged_in
+    end
+
+    def require_logged_out
+    end
+
+    def login(user)
+        # resets the session token & saves new one into cookies
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def logout!
+    end
 end
--- a:A Week 7/W7D1/99Cats-auth/config/routes.rb
+++ a:A Week 7/W7D1/99Cats-auth/config/routes.rb
@@ -11,6 +11,25 @@ Rails.application.routes.draw do
   root to: redirect('/cats')
 
   resources :users, only: [:new,:create]
-
+  resource :sessions, only: [:new, :create, :destroy]
   
 end
+
+  #                   Prefix Verb   URI Pattern                                                  Controller#Action
+  #                     cats GET    /cats(.:format)                                              cats#index
+  #                           POST   /cats(.:format)                                              cats#create
+  #                   new_cat GET    /cats/new(.:format)                                          cats#new
+  #                 edit_cat GET    /cats/:id/edit(.:format)                                     cats#edit
+  #                       cat GET    /cats/:id(.:format)                                          cats#show
+  #                           PATCH  /cats/:id(.:format)                                          cats#update
+  #                           PUT    /cats/:id(.:format)                                          cats#update
+  # approve_cat_rental_request POST   /cat_rental_requests/:id/approve(.:format)                   cat_rental_requests#approve
+  #   deny_cat_rental_request POST   /cat_rental_requests/:id/deny(.:format)                      cat_rental_requests#deny
+  #       cat_rental_requests POST   /cat_rental_requests(.:format)                               cat_rental_requests#create
+  #   new_cat_rental_request GET    /cat_rental_requests/new(.:format)                           cat_rental_requests#new
+  #                     root GET    /                                                            redirect(301, /cats)
+  #                     users POST   /users(.:format)                                         users#create
+  #                 new_user GET    /users/new(.:format)                                     users#new
+  #             new_sessions GET    /sessions/new(.:format)                              sessions#new
+  #                 sessions DELETE /sessions(.:format)                                  sessions#destroy
+  #                           POST   /sessions(.:format)                                  sessions#create
