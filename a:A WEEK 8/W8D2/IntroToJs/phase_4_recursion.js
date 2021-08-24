9eaeb7ce67154ac4a3551771ec938a328d5c786a
your name here <your_email_hereroelliga@yahoo.com>
2021-08-24 16:47:41 -0700

    recursion

--- a:A WEEK 8/W8D2/IntroToJs/phase_4_recursion.js
+++ a:A WEEK 8/W8D2/IntroToJs/phase_4_recursion.js
@@ -9,14 +9,36 @@ function range(start,end){
 }
 
 function sumRec(arr){
-  if(arr.length <= 1){
-    return arr
+  if(arr.length === 1){
+    return arr[0];
   }
-  let val = arr[0] + sumRec(arr.slice(1,arr.length-1))
-  return val
+
+  let val = arr[0]
+  let myArr = val + sumRec(arr.slice(1, arr.length));
+  return myArr;
 
 }
 
 function exponent(base,exp){
+  if(exp === 0){
+    return 1;
+  }
 
+  let val = base * exponent(base, exp - 1);
+  return val;
+}
+
+function exponent2(base, exp){
+  if(exp === 0){
+    return 1;
+  } else if(exp === 1){
+    return base;
+  }
+  let val;
+  if(exp % 2 === 0){
+    val = exponent2(base, exp / 2) ** 2
+  } else {
+    val = base * (exponent2(base, (exp - 1)/ 2) ** 2)
+  }
+  return val;
 }
