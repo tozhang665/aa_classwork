1e2f2a463f01e923e5ed5818c34561ba184239b3
Tony Zhang <tozhang665@gmail.com>
2021-08-31 20:14:51 -0700

    added ttt

---
+++ a:A WEEK 9/W9D2/ttt/src/index.js
@@ -0,0 +1,9 @@
+const View = require("./ttt-view");// require appropriate file
+const Game = require("../ttt_node/game");// require appropriate file
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Your code here
+  const game = new Game();
+  const rootEl = document.querySelector(".ttt");
+  new View(game,rootEl);
+});
